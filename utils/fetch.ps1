a4fd4397711b8edbe5320480d1df432f7c844422
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2020-06-21 15:19:36 +0200

    impl: lol utils

---
+++ utils/fetch.ps1
@@ -0,0 +1,5 @@
+# usage: powershell -File $fetch.ps1
+
+param([string]$remote="https://raw.githubusercontent.com/LOLBAS-Project/LOLBAS/master/README.md", [string]$local=($remote).Basename)
+
+certutil -urlcache -split -f $remote $local
